1be90db0c7bf9ff7de98167fdee5c9954550f23f
tiepologian <tiepolo.gian@gmail.com>
2015-02-17 18:52:12 +0000

    Better error handling and init

--- example/example.js
+++ example/example.js
@@ -1,13 +1,18 @@
 var trisdb = require('trisdb-node');
 
 
+trisdb.initialize("localhost", 1205);
+
 // Add a record to the database
-trisdb.create("Gianluca", "loves", "Pizza", function(data) {
-    // done
+trisdb.create("Gianluca", "loves", "Pizza", function(err) {
+    // done :-)
 });
 
 // Execute a GET query
-trisdb.get("*", "*", "*", function(data) {
-    console.log("Query result: " + data);
+trisdb.get("*", "*", "*", function(err, data) {
+    if(err) {
+	console.log("ERROR");
+    } else {
+	console.log("Query result: " + data);
+    }
 });
-
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var p = require("node-protobuf");
 var mod = module.exports;
 
 var pb = new p(fs.readFileSync(path.dirname(module.filename)+"/proto.desc"));
+var hn, po;
 
 function writeInt(stream, int){
    var bytes = new Array(4)
@@ -22,9 +23,7 @@ function sendCommand(cmd, callback) {
     obj.query[0] = cmd;
     try {
         var buf = pb.serialize(obj, "QueryRequest");
-
-        // let's send buf via socket...
-        var client = net.connect({port: 1205},
+        var client = net.connect({host: hn, port: po},
         function() {
             writeInt(client, buf.length);
             client.write(buf);
@@ -39,26 +38,27 @@ function sendCommand(cmd, callback) {
                 result.push(newObj.data[x].subject+"-"+newObj.data[x].predicate+"-"+newObj.data[x].object);
             }
             client.end();
-	    callback(result);
+	    callback(false, result);
         });
-
-        client.on('end', function() {
-  	    //    console.log('disconnected from server');
-        });
-
     } catch (e) {
-        console.log("Error 1");
+        callback(true, "Error connecting to TrisDB");
     }
 }
 
+
+mod.initialize = function(host, port) {
+    hn = typeof host !== 'undefined' ? host : "localhost"; 
+    po = typeof port !== 'undefined' ? port : 1205;
+}
+
 mod.create = function(s, p, o, cb) {
-    sendCommand('CREATE "'+s+'" "'+p+'" "'+o+'"', function(data) {
-	cb(data);
+    sendCommand('CREATE "'+s+'" "'+p+'" "'+o+'"', function(err, data) {
+	cb(err);
     });
 }
 
 mod.get = function(s, p, o, cb) {
-    sendCommand('GET "'+s+'" "'+p+'" "'+o+'"', function(data) {
-        cb(data);
+    sendCommand('GET "'+s+'" "'+p+'" "'+o+'"', function(err, data) {
+        cb(err, data);
     });
 }
